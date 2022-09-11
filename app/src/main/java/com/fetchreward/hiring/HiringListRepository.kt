a2188ed8618b14a99786fec9660dd569440ecc53
Jae Jeong <mjj0404@gmail.com>
2022-09-11 00:41:10 -0400

    implement data filters

--- app/src/main/java/com/fetchreward/hiring/HiringListRepository.kt
+++ app/src/main/java/com/fetchreward/hiring/HiringListRepository.kt
@@ -1,4 +1,24 @@
 package com.fetchreward.hiring
 
+import com.fetchreward.hiring.api.RetrofitService
+import com.fetchreward.hiring.model.HiringItem
+
 class HiringListRepository {
+
+    suspend fun fetchHiringItems(): List<HiringItem> {
+        val response = RetrofitService.getInstance().fetchHiringItems()
+        val hiringList: List<HiringItem>
+        return if (response.isSuccessful) {
+            hiringList = response.body()!!.filter { !it.name.isNullOrBlank() }
+            sort(hiringList)
+        } else {
+            emptyList()
+        }
+    }
+
+    private fun sort(hiringList: List<HiringItem>): List<HiringItem> {
+        return hiringList.sortedWith(compareBy<HiringItem> {item -> item.listId}
+            .thenBy { item -> item.name.filter { it.isDigit() } }
+        )
+    }
 }
